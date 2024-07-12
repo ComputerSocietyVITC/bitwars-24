77bb4e04606dc42a1ac30bc1f410bb31568b0b5f
Ananta Krishnan B <anantukid@gmail.com>
2024-07-12 13:52:48 +0530

    chore: added dummy text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { Poppins } from "next/font/google";
-import Timer from "@/components/timer/Timer"
+import Timer from "@/components/timer/Timer";
 
 const poppins = Poppins({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
@@ -22,7 +22,25 @@ export default function Home() {
         <Timer />
         <div className="max-w-screen-2xl h-screen">
           <p className="mt-24 font-[1000] text-5xl">ABOUT US</p>
-          <div className="w-2/4 h-80 ml-20 mt-6 rounded-2xl bg-[radial-gradient(_farthest-corner_at_40px_40px,_#104456_0%,_#100e17_100%_)] text-center"></div>
+          <div className="w-2/4 h-80 ml-20 mt-6 rounded-2xl bg-[radial-gradient(_farthest-corner_at_40px_40px,_#104456_0%,_#100e17_100%_)] text-center">
+            <p>
+              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industrys standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make a
+              type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially
+              unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently
+              with desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum. Why do we use it? It is a long
+              established fact that a reader will be distracted by the readable
+              content of a page when looking at its layout. The point of using
+              Lorem Ipsum is that it has a more-or-less normal distribution of
+              letters, as opposed to using Content here, content here, making it
+              look like readable English.
+            </p>
+          </div>
         </div>
         <p>
           What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
