14ee5d8e237e90f9c8840e347607af09db9687aa
Ananta Krishnan B <anantukid@gmail.com>
2024-07-12 22:45:12 +0530

    chore: removed react-icon and replaced the container with a flex

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,8 @@ import TextButton from "../buttons/TextButton";
 import SocialMediaGrid from "./SocialsGrid";
 import React from "react";
 import Image from "next/image";
-import { FaArrowUpLong } from "react-icons/fa6";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpLong } from "@fortawesome/free-solid-svg-icons";
 
 const Footer: React.FC = () => {
   const scrollToTop = () => {
@@ -14,32 +15,26 @@ const Footer: React.FC = () => {
       <footer className="text-white p-8">
         {/* Scroll to top button and line */}
         <div className="flex justify-center items-center mb-4">
-          <hr
-            className="absolute w-full border-t border-gray-700"
-            style={{
-              backgroundImage:
-                "linear-gradient(to right, #0F1D29, #114554 50%, #0F1D29)",
-            }}
-          />
+          <hr className="absolute w-4/5 h-1 border-gray-700 bg-gradient-to-r from-[#0F1D29] via-[#114554] to-[#0F1D29] opacity-50" />
           <button
             onClick={scrollToTop}
-            className="relative z-10 flex items-center bg-[#0B1E3E] rounded-full w-10 h-10 hover:bg-blue-500 transition-all duration-300"
+            className="relative z-10 flex items-center justify-center bg-[#0B1E3E] rounded-full w-10 h-10 hover:bg-blue-500 transition-all duration-300"
           >
-            <FaArrowUpLong size={28} className="pl-3" />
+            <FontAwesomeIcon icon={faArrowUpLong} size="lg" />
           </button>
         </div>
 
         {/* Footer content */}
-        <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
+        <div className="flex md:flex-row flex-col justify-center gap-52">
           {/* IEEE logo */}
-          <div className="flex items-center">
+          <div className="flex items-center justify-center">
             <Image src="/logo2.png" alt="logo2" width={280} height={80} />
           </div>
 
           {/* Menu */}
-          <div className="space-y-4">
+          <div className="space-y-4 justify-center items-center">
             <h3 className="text-xl font-semibold items-center">Menu</h3>
-            <ul className="space-y-2">
+            <ul className="space-y-2 text-left">
               <TextButton href="#about">About</TextButton>
               <TextButton href="#team">Our Team</TextButton>
               <TextButton href="#events">Events</TextButton>
@@ -49,9 +44,9 @@ const Footer: React.FC = () => {
           </div>
 
           {/* VITC address */}
-          <div className="space-y-4">
+          <div className="space-y-4 text-center justify-center">
             <h3 className="text-xl font-semibold">Address</h3>
-            <p>
+            <p className="text-left">
               Kelambakkam - Vandallur
               <br />
               Rd, Rajan Nagar, Chennai,
@@ -62,8 +57,8 @@ const Footer: React.FC = () => {
           </div>
 
           {/* Social Media */}
-          <div className="space-y-4 text-center">
-            <h3 className="text-xl font-semibold">Follow Us</h3>
+          <div className="space-y-4 text-center justify-center">
+            <h3 className="text-xl font-semibold">Social Media</h3>
             <SocialMediaGrid />
           </div>
         </div>
