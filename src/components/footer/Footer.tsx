a162441c55b228594126e4a7c0739d4b96adfc08
Ananta Krishnan B <anantukid@gmail.com>
2024-07-13 00:00:24 +0530

    chore: made footer line transparent

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <footer className="text-white p-8">
         {/* Scroll to top button and line */}
         <div className="flex justify-center items-center mb-4">
-          <hr className="absolute w-4/5 h-1 border-gray-700 bg-gradient-to-r from-[#0F1D29] via-[#114554] to-[#0F1D29] opacity-50" />
+          <hr className="absolute w-full h-1 border-transparent bg-gradient-to-r from-transparent via-[#114554]" />
           <Link
             href="#top"
             className="relative z-10 flex items-center justify-center bg-[#0B1E3E] rounded-full w-10 h-10 hover:bg-blue-500 transition-all duration-300"
@@ -22,14 +22,14 @@ const Footer = () => {
         </div>
 
         {/* Footer content */}
-        <div className="flex md:flex-row flex-col justify-center gap-52">
+        <div className="flex md:flex-row flex-col justify-center gap-40">
           {/* IEEE logo */}
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center basis-1/3">
             <Image src="/logo2.png" alt="logo2" width={280} height={80} />
           </div>
 
           {/* Menu */}
-          <div className="space-y-4 justify-center items-center">
+          <div className="space-y-4 justify-center items-center basis-1/6">
             <h3 className="text-xl font-semibold items-center">Menu</h3>
             <ul className="space-y-2 text-left">
               <TextButton href="#about">About</TextButton>
@@ -41,7 +41,7 @@ const Footer = () => {
           </div>
 
           {/* VITC address */}
-          <div className="space-y-4 text-center justify-center">
+          <div className="space-y-4 text-center justify-center basis-1/5">
             <h3 className="text-xl font-semibold">Address</h3>
             <p className="text-left">
               Kelambakkam - Vandallur
@@ -54,7 +54,7 @@ const Footer = () => {
           </div>
 
           {/* Social Media */}
-          <div className="space-y-4 text-center justify-center">
+          <div className="space-y-4 text-center justify-center basis-1/4">
             <h3 className="text-xl font-semibold">Social Media</h3>
             <SocialMediaGrid />
           </div>
