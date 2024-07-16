35fe945253879c46b8a74817b2a18a6e70c77064
shotoyaar <anirudhsridhar1064@gmail.com>
2024-07-16 14:32:42 +0530

    feat: added image display and events section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,11 @@ export default function Home() {
           className="z-30 items-center justify-center align-center mx-auto mt-24"
         />
         <Timer />
+<<<<<<< HEAD
         <div className="w-full h-screen" id="about">
+=======
+        <div className="w-full">
+>>>>>>> 21b1d44 (feat: added image display and events section.)
           <p className="mt-24 ml-36 font-extrabold text-5xl">ABOUT US</p>
           <div className="w-2/4 h-80 ml-48 mt-6 rounded-2xl bg-[radial-gradient(_farthest-corner_at_10px_10px,_#104456_0%,_#100e17_100%_)] text-center">
             <p className="text-white">Lorem ipsum dolor set</p>
@@ -29,6 +33,38 @@ export default function Home() {
           <p className="mt-24 text-center font-extrabold text-5xl">
             Our Event Showcase
           </p>
+          <div className="flex flex-row w-full">
+            <section className="h-min flex items-left pt-12 pl-12 pr-24 pb-48 my-12 mr-24">
+              <section className="flex flex-col mx-auto">
+                <section className="flex">
+                  <Image
+                    src="/logo.png"
+                    alt="image1"
+                    width={150}
+                    height={150}
+                    className="rounded-3xl"
+                  />
+
+                  <Image
+                    alt="image2"
+                    src="/logo.png"
+                    height={150}
+                    width={150}
+                    className="rounded-3xl -translate-x-8 translate-y-12"
+                  />
+                </section>
+                <Image
+                  alt="image3"
+                  src="/logo.png"
+                  height={150}
+                  width={150}
+                  className="rounded-3xl z-10 translate-x-12 -translate-y-8"
+                />
+              </section>
+            </section>
+
+            <section className="flex w-2/4 h-80 mt-12 rounded-2xl bg-[radial-gradient(_farthest-corner_at_60px_60px,_#100e17_0%,_#104456_100%)] text-center"></section>
+          </div>
         </div>
         <p>
           What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
