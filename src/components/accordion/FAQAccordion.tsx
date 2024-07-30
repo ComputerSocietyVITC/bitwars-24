c02bc465d1c07a9227296feaff998c5e9b650e1c
Ananta Krishnan B <anantukid@gmail.com>
2024-07-30 11:18:33 +0530

    revert: revert commit

--- src/components/accordion/FAQAccordion.tsx
+++ src/components/accordion/FAQAccordion.tsx
@@ -19,18 +19,16 @@ const FAQAccordion = () => {
         <div className="px-4">
           <AccordionItem value="item-1">
             <div className="w-[100%] accord-lines h-0.5"></div>
-            <AccordionTrigger className="text-white">
+            <AccordionTrigger className="text-white ">
               Lorem ipsum
             </AccordionTrigger>
             <AccordionContent className="text-white text-left accord-cont p-4 leading-relaxed">
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry&apos;s standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting.
-              </p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
@@ -39,15 +37,13 @@ const FAQAccordion = () => {
               Lorem ipsum
             </AccordionTrigger>
             <AccordionContent className="text-white text-left accord-cont p-4 leading-relaxed">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-                aperiam, sapiente nulla labore voluptas culpa incidunt
-                perferendis necessitatibus corrupti debitis ipsum ullam odio
-                hic! Ipsum laborum eligendi provident, voluptatum odit at
-                nesciunt eveniet repellendus officia! Quae, vitae animi quidem
-                repellat provident in nobis quis quisquam, dolores perferendis,
-                totam voluptate numquam.
-              </p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              aperiam, sapiente nulla labore voluptas culpa incidunt perferendis
+              necessitatibus corrupti debitis ipsum ullam odio hic! Ipsum
+              laborum eligendi provident, voluptatum odit at nesciunt eveniet
+              repellendus officia! Quae, vitae animi quidem repellat provident
+              in nobis quis quisquam, dolores perferendis, totam voluptate
+              numquam.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">
@@ -57,15 +53,12 @@ const FAQAccordion = () => {
             </AccordionTrigger>
             <div className="w-[100%] accord-lines h-0.5"></div>
             <AccordionContent className="text-white text-left accord-cont p-4 leading-relaxed">
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
-                iure quidem id eveniet nemo? Deserunt repellendus nulla dolorum
-                cum, beatae, quisquam vero voluptatibus nesciunt soluta totam
-                cumque sint autem quis dolor facere! Autem, provident animi
-                ipsum voluptatibus perferendis magni modi enim reiciendis
-                dignissimos soluta porro adipisci voluptate corrupti neque
-                aliquid.
-              </p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
+              iure quidem id eveniet nemo? Deserunt repellendus nulla dolorum
+              cum, beatae, quisquam vero voluptatibus nesciunt soluta totam
+              cumque sint autem quis dolor facere! Autem, provident animi ipsum
+              voluptatibus perferendis magni modi enim reiciendis dignissimos
+              soluta porro adipisci voluptate corrupti neque aliquid.
             </AccordionContent>
           </AccordionItem>
         </div>
