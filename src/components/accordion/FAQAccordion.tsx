b0386876253623cd8daf784961ba0e3212d240b7
Ananta Krishnan B <anantukid@gmail.com>
2024-07-30 11:11:32 +0530

    fix: trying to fix text render again

--- src/components/accordion/FAQAccordion.tsx
+++ src/components/accordion/FAQAccordion.tsx
@@ -19,17 +19,17 @@ const FAQAccordion = () => {
         <div className="px-4">
           <AccordionItem value="item-1">
             <div className="w-[100%] accord-lines h-0.5"></div>
-            <AccordionTrigger className="text-white ">
+            <AccordionTrigger className="text-white">
               Lorem ipsum
             </AccordionTrigger>
             <AccordionContent className="text-white text-left accord-cont p-4 leading-relaxed">
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
+                industry. Lorem Ipsum has been the industry&apos;s standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
                 of type and scrambled it to make a type specimen book. It has
                 survived not only five centuries, but also the leap into
-                electronic typesetting.{" "}
+                electronic typesetting.
               </p>
             </AccordionContent>
           </AccordionItem>
@@ -46,7 +46,7 @@ const FAQAccordion = () => {
                 hic! Ipsum laborum eligendi provident, voluptatum odit at
                 nesciunt eveniet repellendus officia! Quae, vitae animi quidem
                 repellat provident in nobis quis quisquam, dolores perferendis,
-                totam voluptate numquam.{" "}
+                totam voluptate numquam.
               </p>
             </AccordionContent>
           </AccordionItem>
