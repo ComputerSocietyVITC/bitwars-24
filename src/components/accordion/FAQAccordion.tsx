4e6f96594f25c03f7bd799480e390947133a3098
Ananta Krishnan B <anantukid@gmail.com>
2024-07-30 11:08:17 +0530

    fix: trying to fix text rendering issue on prod

--- src/components/accordion/FAQAccordion.tsx
+++ src/components/accordion/FAQAccordion.tsx
@@ -23,12 +23,14 @@ const FAQAccordion = () => {
               Lorem ipsum
             </AccordionTrigger>
             <AccordionContent className="text-white text-left accord-cont p-4 leading-relaxed">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry&apos;s standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting.
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting.{" "}
+              </p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
@@ -37,7 +39,15 @@ const FAQAccordion = () => {
               Lorem ipsum
             </AccordionTrigger>
             <AccordionContent className="text-white text-left accord-cont p-4 leading-relaxed">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam aperiam, sapiente nulla labore voluptas culpa incidunt perferendis necessitatibus corrupti debitis ipsum ullam odio hic! Ipsum laborum eligendi provident, voluptatum odit at nesciunt eveniet repellendus officia! Quae, vitae animi quidem repellat provident in nobis quis quisquam, dolores perferendis, totam voluptate numquam.
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                aperiam, sapiente nulla labore voluptas culpa incidunt
+                perferendis necessitatibus corrupti debitis ipsum ullam odio
+                hic! Ipsum laborum eligendi provident, voluptatum odit at
+                nesciunt eveniet repellendus officia! Quae, vitae animi quidem
+                repellat provident in nobis quis quisquam, dolores perferendis,
+                totam voluptate numquam.{" "}
+              </p>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">
@@ -47,7 +57,15 @@ const FAQAccordion = () => {
             </AccordionTrigger>
             <div className="w-[100%] accord-lines h-0.5"></div>
             <AccordionContent className="text-white text-left accord-cont p-4 leading-relaxed">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae iure quidem id eveniet nemo? Deserunt repellendus nulla dolorum cum, beatae, quisquam vero voluptatibus nesciunt soluta totam cumque sint autem quis dolor facere! Autem, provident animi ipsum voluptatibus perferendis magni modi enim reiciendis dignissimos soluta porro adipisci voluptate corrupti neque aliquid.
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
+                iure quidem id eveniet nemo? Deserunt repellendus nulla dolorum
+                cum, beatae, quisquam vero voluptatibus nesciunt soluta totam
+                cumque sint autem quis dolor facere! Autem, provident animi
+                ipsum voluptatibus perferendis magni modi enim reiciendis
+                dignissimos soluta porro adipisci voluptate corrupti neque
+                aliquid.
+              </p>
             </AccordionContent>
           </AccordionItem>
         </div>
