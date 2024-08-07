55be19c09e7cc637b9a6a89dc74a484351a20224
Ananta Krishnan B <anantukid@gmail.com>
2024-08-07 13:18:08 +0530

    chore: made the with smaller for better responsiveness

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -26,7 +26,7 @@ const Timer = () => {
   }, []);
 
   return (
-    <div className="w-3/5 md:w-full max-w-screen-lg h-full bg-gradient-to-r from-[#14357A] via-[#1088AC] to-[#1083A9] rounded-3xl skew-x-[-25deg] mx-auto mt-[-30px] partial-border-gradient">
+    <div className="w-1/2 md:w-full max-w-screen-lg h-full bg-gradient-to-r from-[#14357A] via-[#1088AC] to-[#1083A9] rounded-3xl skew-x-[-25deg] mx-auto mt-[-30px] partial-border-gradient">
       <div className="h-fit w-full timer-bg rounded-xl text-white text-4xl md:text-8xl inline-block text-center p-4 md:p-8">
         <div className="flex flex-col md:flex-row justify-around">
           <div className="w-full md:w-1/4 mb-4 md:mb-0">
