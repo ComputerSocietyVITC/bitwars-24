46566761f54f9be7cb2acf9ae3c4d9e49e7e845c
sam-shervin <samshervin.sebastian@gmail.com>
2024-08-09 13:39:29 +0530

    timer lg done

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -28,29 +28,35 @@ const Timer = () => {
   return (
     <div className="w-4/5 md:w-full max-w-screen-lg h-full bg-gradient-to-r from-[#14357A] via-[#1088AC] to-[#1083A9] rounded-3xl skew-x-[-25deg] md:skew-x-[-25deg] sm:skew-x-[-15deg] mx-auto partial-border-gradient">
       <div className="h-fit w-full timer-bg rounded-xl text-white sm:text-4xl md:text-4xl text-8xl inline-block text-center p-4 md:p-8">
-        <div className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-2">
-          <div className="w-full">
-            <div className="skew-x-[25deg]">
+        <div className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-2 items-center">
+          <div className="w-full mt-4">
+            <div className="skew-x-[25deg] w-full">
               <p id="day" className="-mb-2 font-extrabold">
                 {days < 10 ? "0" + days : days}
               </p>
-              <span className="text-4xl md:text-3xl font-thin">DAYS</span>
+              <section className="text-4xl md:text-3xl font-thin py-4">
+                DAYS
+              </section>
             </div>
           </div>
-          <div className="w-full mb-4">
+          <div className="w-full">
             <div className="skew-x-[25deg]">
               <p id="hour" className="-mb-2 font-extrabold">
                 {hours < 10 ? "0" + hours : hours}
               </p>
-              <span className="text-4xl md:text-3xl font-thin">HOURS</span>
+              <section className="text-4xl md:text-3xl font-thin py-4">
+                HOURS
+              </section>
             </div>
           </div>
-          <div className="w-full mb-4 =">
+          <div className="w-full">
             <div className="skew-x-[25deg]">
               <p id="minute" className="-mb-2 font-extrabold">
                 {minutes < 10 ? "0" + minutes : minutes}
               </p>
-              <span className="text-4xl md:text-3xl font-thin">MINUTES</span>
+              <section className="text-4xl md:text-3xl font-thin py-4">
+                MINUTES
+              </section>
             </div>
           </div>
           <div className="w-full">
@@ -58,7 +64,9 @@ const Timer = () => {
               <p id="second" className="-mb-2 font-extrabold">
                 {seconds < 10 ? "0" + seconds : seconds}
               </p>
-              <span className="text-4xl md:text-3xl font-thin">SECONDS</span>
+              <section className="text-4xl md:text-3xl font-thin py-4">
+                SECONDS
+              </section>
             </div>
           </div>
         </div>
