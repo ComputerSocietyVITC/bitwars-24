253c091113a4831f5cf287319bf3cabf0c7e3649
sam-shervin <samshervin.sebastian@gmail.com>
2024-08-09 16:39:20 +0530

    chore: Update image sizes and styles for better responsiveness

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Aboutus = () => {
   return (
     <div className="w-full" id="about">
-      <div className="w-full flex flex-col mb-24">
+      <div className="w-full flex flex-col">
         <p className="mt-24 font-extrabold text-5xl text-center">About Us</p>
         <div className="flex gap-[5vw] px-12 py-12 items-center">
           <div className="shadow-md shadow-[#292761] flex flex-col items-center w-full rounded-2xl text-center bg-gradient-to-br from-[#100e1773] via-[#10345676] to-[#100e177e] border border-white/[0.2]">
