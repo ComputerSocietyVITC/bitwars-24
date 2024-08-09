272080a48307e9b4649fa4c5d2cc4effea56837a
sam-shervin <samshervin.sebastian@gmail.com>
2024-08-09 05:41:27 +0530

    refactor: Improve responsiveness of Timer component

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -28,13 +28,13 @@ const Timer = () => {
   return (
     <div className="w-4/5 md:w-full max-w-screen-lg h-full bg-gradient-to-r from-[#14357A] via-[#1088AC] to-[#1083A9] rounded-3xl skew-x-[-25deg] md:skew-x-[-25deg] sm:skew-x-[-15deg] mx-auto partial-border-gradient">
       <div className="h-fit w-full timer-bg rounded-xl text-white sm:text-4xl md:text-4xl text-8xl inline-block text-center p-4 md:p-8">
-        <div className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-2 justify-around">
-          <div className="w-full mb-4">
+        <div className="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-2">
+          <div className="w-full">
             <div className="skew-x-[25deg]">
               <p id="day" className="-mb-2 font-extrabold">
                 {days < 10 ? "0" + days : days}
               </p>
-              <span className="text-lg md:text-3xl font-thin">DAYS</span>
+              <span className="text-4xl md:text-3xl font-thin">DAYS</span>
             </div>
           </div>
           <div className="w-full mb-4">
@@ -42,7 +42,7 @@ const Timer = () => {
               <p id="hour" className="-mb-2 font-extrabold">
                 {hours < 10 ? "0" + hours : hours}
               </p>
-              <span className="text-lg md:text-3xl font-thin">HOURS</span>
+              <span className="text-4xl md:text-3xl font-thin">HOURS</span>
             </div>
           </div>
           <div className="w-full mb-4 =">
@@ -50,7 +50,7 @@ const Timer = () => {
               <p id="minute" className="-mb-2 font-extrabold">
                 {minutes < 10 ? "0" + minutes : minutes}
               </p>
-              <span className="text-lg md:text-3xl font-thin">MINUTES</span>
+              <span className="text-4xl md:text-3xl font-thin">MINUTES</span>
             </div>
           </div>
           <div className="w-full">
@@ -58,7 +58,7 @@ const Timer = () => {
               <p id="second" className="-mb-2 font-extrabold">
                 {seconds < 10 ? "0" + seconds : seconds}
               </p>
-              <span className="text-lg md:text-3xl font-thin">SECONDS</span>
+              <span className="text-4xl md:text-3xl font-thin">SECONDS</span>
             </div>
           </div>
         </div>
