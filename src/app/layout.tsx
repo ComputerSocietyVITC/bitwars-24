795d170bfe894d846060d7638dfcf94279ab1f48
sam-shervin <samshervin.sebastian@gmail.com>
2024-08-10 10:37:41 +0530

    feat: Add home link to navigation bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,10 @@ export default function RootLayout({
 }>) {
   const navItems = [
     {
+      name: "Home",
+      link: "#home",
+    },
+    {
       name: "About",
       link: "#about",
     },
