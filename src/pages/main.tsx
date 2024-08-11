f58e7b20db7586f8f8c71b3611c19d76f151674d
sam-shervin <samshervin.sebastian@gmail.com>
2024-08-11 09:20:39 +0530

    immediate push due to logo alignmenmt

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -4,45 +4,67 @@ import Timer from "@/components/timer/Timer";
 
 const Main: React.FC = () => {
   return (
-    <section className="h-screen">
-      <section className="w-full flex -mb-[6vh] justify-around">
+    <section className="h-screen grid">
+      <section className="w-full grid grid-cols-3 place-items-center">
         <Image
           src="/vit.svg"
           alt=""
-          width={150}
-          height={100}
-          className="object-contain w-[17vh] sm:w-28"
+          width={500}
+          height={500}
+          className="object-contain h-[7vh] w-full sm:w-28"
         />
         <Image
           src="/compsoc.png"
           width={150}
           height={100}
           alt=""
-          className=" object-contain w-[17vh] sm:w-28 sm:pl-5 ml-8"
+          className=" object-contain h-[7vh] w-full ml-6 sm:pl-5"
         />
         <Image
-          src="/swc.png"
+          src="/swc.svg"
           width={150}
           height={100}
           alt=""
-          className="object-contain w-[25vh] sm:w-28"
+          className="object-contain h-[5vh] w-full sm:w-28"
         />
       </section>
-      <div className="h-[60%] flex items-center">
-        <div
-          className=" self-center mx-auto  w-4/5 flex flex-col scale-[95%] mt-12"
-          id="home"
-        >
-          <Image
-            src="/bitwarsLogo.png"
-            alt="logo"
-            height={500}
-            width={500}
-            className="z-30 justify-center align-center mx-auto relative top-10 w-[45vh] md:scale-[80%] md:top-24"
-          />
-          <Timer />
+      <div className="grid-cols-1 place-self-center w-full">
+        <div className="flex items-center ">
+          <div className=" self-center mx-auto  w-4/5 flex flex-col" id="home">
+            <Image
+              src="/bitwarsLogo.png"
+              alt="logo"
+              height={500}
+              width={500}
+              className="z-30 justify-center align-center mx-auto relative top-10 w-[45vh] md:scale-[80%] md:top-24"
+            />
+            <Timer />
+          </div>
         </div>
       </div>
+      <section className=" w-full grid grid-cols-3 place-items-center invisible">
+        <Image
+          src="/vit.svg"
+          alt=""
+          width={500}
+          height={500}
+          className="object-contain h-[7vh] w-full sm:w-28"
+        />
+        <Image
+          src="/compsoc.png"
+          width={150}
+          height={100}
+          alt=""
+          className=" object-contain h-[7vh] w-full ml-6 sm:pl-5"
+        />
+        <Image
+          src="/swc.svg"
+          width={150}
+          height={100}
+          alt=""
+          className="object-contain h-[5vh] w-full sm:w-28"
+        />
+      </section>
     </section>
   );
 };
